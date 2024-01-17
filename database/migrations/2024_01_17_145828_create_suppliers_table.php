ff03eba38a3e869f25c54914217fd5be66ac60bc
DevDanielAlvarez <skyrimdaniel11@gmail.com>
2024-01-17 19:41:38 -0300

    feat: migrate, factory and seeder

--- database/migrations/2024_01_17_145828_create_suppliers_table.php
+++ database/migrations/2024_01_17_145828_create_suppliers_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\StatusSupplierEnum ;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,9 @@ return new class extends Migration
     {
         Schema::create('suppliers', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->enum('status',array_column(StatusSupplierEnum::cases(),"name"))
+            ->default(StatusSupplierEnum::i->name);
             $table->timestamps();
         });
     }
